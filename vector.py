b0086c5c84cadaac896e60990022ee858964a9e7
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-06-18 17:15:05 -0300

    added code

--- vector.py
+++ vector.py
@@ -1,38 +1,42 @@
 """Módulo com as funções de manipulação de vetores."""
-
+import math
+from typing import List
 
 def norma(x: list[float]) -> float | None:
     """Calcula a norma de um vetor"""
-    # TODO: implementar
-    # a norma de um vetor [3, 4] é 5
-    # a norma de um vetor [1, 2, 4] é 4.58257569495584
-    # ela consiste em calcular a raiz quadrada da soma dos quadrados dos elementos do vetor
-    # se o vetor estiver vazio retorne None
-
+    if x == 0:
+      return None
+    quadrado_soma = 0.0
+    for element in x:
+        quadrado_soma += element ** 2
+    norma = math.sqrt(quadrado_soma)
+    return norma
 
 def soma(x: list[float], y: list[float]) -> list[float] | None:
     """Soma dois vetores"""
-    # TODO: implementar
-    # a soma de dois vetores [1, 2, 4] + [2, 3, 4] é [3, 5, 8]
-    # a soma só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
-
+    if len(x) != len(y) :
+        return None
+    soma_vetores = []
+    for i in range(len(x)):
+        soma_vetores.append(x[i] + y[i])
+    return soma_vetores
 
 def multiplicação_por_escalar(vetor: list[float], escalar: float) -> list[float]:
     """Multiplica um vetor por um escalar"""
+    vetor_multiplicado = [element * escalar for element in vetor]
+    return vetor_multiplicado
     # TODO: implementar
     # a multiplicação de um vetor [1, 2, 4] por um escalar 2 é [2, 4, 8]
 
 
 def produto_interno(x: list[float], y: list[float]) -> float | None:
     """Calcula o produto interno de dois vetores"""
-    # TODO: implementar
-    # o produto interno de dois vetores [1, 2, 4] e [2, 3, 4] é 24
-    # o produto interno consiste em multiplicar os elementos de mesmo índice e somar os resultados
-    # a multiplicação só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
-    # caso os vetores sejam vazios o resultado é 0
-
+    if len(x) != len(y):
+        return None
+    produto_vetores = 0.0
+    for i in range(len(x)):
+        produto_vetores += x[i] * y[i]
+    return produto_vetores
 
 def produto_vetorial(x: list[float], y: list[float]) -> list[float] | None:
     """Calcula o produto vetorial de dois vetores"""
