040866fceeffe1a107b1cd68f44f4f5e7f7b457c
Artur Paschoal <128155308+paschoal-artur@users.noreply.github.com>
2023-06-30 16:23:16 +0000

    added formatted spaces

--- matrix.py
+++ matrix.py
@@ -11,12 +11,16 @@ def soma(x: Matriz, y: Matriz) -> Matriz | None:
         return []
     if len(x) != len(y) or len(x[0]) != len(y[0]):
         return None
+
     rows = len(x)
     columns = len(x[0])
+
     soma_matrizes = [[0.0] * columns for _ in range(rows)]
+
     for i in range(rows):
         for j in range(columns):
             soma_matrizes[i][j] = x[i][j] + y[i][j]
+
     return soma_matrizes
 
 
@@ -24,12 +28,16 @@ def multiplicacao_por_escalar(matriz: Matriz, escalar: Escalar) -> Matriz:
     """Multiplicando por um escalar"""
     if len(matriz) == 0 or len(matriz[0]) == 0:
         return []
+
     rows = len(matriz)
     columns = len(matriz[0])
+
     conta = [[0.0] * columns for _ in range(rows)]
+
     for i in range(rows):
         for j in range(columns):
             conta[i][j] = matriz[i][j] * escalar
+
     return conta
 
 
@@ -37,6 +45,7 @@ def multiplicacao(x: Matriz, y: Matriz) -> Matriz | None:
     """ "Multiplicando matrizes"""
     if len(x) == 0 or len(y) == 0 or len(x[0]) == 0 or len(y[0]) == 0:
         return []
+
     if len(x[0]) != len(y):
         return None
 
@@ -58,22 +67,29 @@ def norma(x: Matriz) -> Escalar:
     """Calcula a norma de uma matriz"""
     if len(x) == [] and len(x[0]) == []:
         return 0
+
     quadrado_soma = 0.0
+
     for i in x:
         for element in i:
             quadrado_soma += element**2
+
     norma_calculada = math.sqrt(quadrado_soma)
+
     return norma_calculada
 
 
 def eh_simetrica(x: Matriz) -> bool:
     """Verifica se uma matriz é simétrica"""
     matrix = np.array(x)
+
     transpose_matrix = np.transpose(matrix)
+
     return np.array_equal(matrix, transpose_matrix)
 
 
 def transposta(x: Matriz) -> Matriz:
     """Calcula a transposta de uma matriz"""
     transposed_matrix = np.transpose(x)
+
     return transposed_matrix.tolist()
--- vector.py
+++ vector.py
@@ -10,10 +10,14 @@ def norma(x: Vetor) -> Escalar | None:
     """Calcula a norma de um vetor"""
     if x == []:
         return None
+
     quadrado_soma = 0.0
+
     for element in x:
         quadrado_soma += element**2
+
     norma_calculada = math.sqrt(quadrado_soma)
+
     return norma_calculada
 
 
@@ -21,15 +25,19 @@ def soma(x: Vetor, y: Vetor) -> Vetor | None:
     """Soma dois vetores"""
     if len(x) != len(y):
         return None
+
     soma_vetores = []
+
     for i, x_i in enumerate(x):
         soma_vetores.append(x_i + y[i])
+
     return soma_vetores
 
 
 def multiplicacao_por_escalar(vetor: Vetor, escalar: Escalar) -> Vetor:
     """Multiplicando por escalar"""
     vetor_multiplicado = [element * escalar for element in vetor]
+
     return vetor_multiplicado
 
 
@@ -37,11 +45,15 @@ def produto_interno(x: Vetor, y: Vetor) -> Escalar | None:
     """Calcula o produto interno de dois vetores"""
     if len(x) != len(y):
         return None
+
     if x == [] and y == []:
         return 0
+
     produto_vetores = 0.0
+
     for i, x_i in enumerate(x):
         produto_vetores += x_i * y[i]
+
     return produto_vetores
 
 
@@ -49,7 +61,9 @@ def produto_vetorial(x: Vetor, y: Vetor) -> Vetor | None:
     """Calcula o produto vetorial de dois vetores"""
     if len(x) != 3 or len(y) != 3:
         return None
+
     resultado = np.cross(x, y)
+
     return resultado.tolist()
 
 
@@ -57,12 +71,16 @@ def produto_diadico(x: Vetor, y: Vetor) -> Matriz | None:
     """Calculo do produto diadico"""
     if len(x) == 0 or len(y) == 0:
         return []
+
     if len(x) != len(y):
         return None
+
     result = []
+
     for x_i in x:
         row = []
         for y_j in y:
             row.append(x_i * y_j)
         result.append(row)
+
     return result
